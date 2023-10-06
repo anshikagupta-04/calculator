2f3194aec6be6bf8f09db3287365e7936eeee14d
anshikagupta-04 <pg8998211@gmail.com>
2023-10-06 18:07:13 +0530

    calculator

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Calculator from './Calculator'
+
+function App() {
+
+  return (
+    <>
+      <Calculator/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Calculator.jsx
@@ -0,0 +1,63 @@
+import React, {useState} from "react";
+
+function Calculator(){
+    // let string="";
+    // let buttons=document.querySelectorAll('.button');
+    // Array.from(buttons).forEach((button)=>{
+    //     button.addEventListener('click', (e)=>{
+    //         if(e.target.innerHTML== '='){
+    //             string= eval(string);
+    //             document.querySelector('input').value = string;
+    //         }
+    //         else {
+    //             console.log(e.target);
+    //             string = string + e.target.innerHTML;
+    //             document.querySelector('input').value = string;
+    //         }
+    //     })
+    // })
+    const [value, setValue] = useState('');
+    
+    return(
+        <>
+            <div className="main">
+                <h1>Calculate me!</h1>
+                <div className="container">
+                    <input type="text" disabled value={value} onChange={(e) => setValue(e.target.value)}/>
+                    <div className="row">
+                        <button className="button" onClick={e => setValue('')}>AC</button>
+                        <button className="button" onClick={e => setValue (r => r.slice(0, r.length - 1))}>CE</button>
+                        <button className="button" onClick={e => setValue(r => `${r}00`)}>00</button>
+                        <button className="button" onClick={e => setValue (r => `${r}*`)}>*</button>
+                    </div>
+                    <div className="row">
+                        <button className="button" onClick={e => setValue(r => `${r}7`)}>7</button>
+                        <button className="button" onClick={e => setValue (r => `${r}8`)}>8</button>
+                        <button className="button" onClick={e => setValue (r => `${r}9`)}>9</button>
+                        <button className="button" onClick={e => setValue (r => `${r}/`)}>/</button>
+                    </div>
+                    <div className="row">
+                        <button className="button" onClick={e => setValue (r => `${r}4`)}>4</button>
+                        <button className="button" onClick={e => setValue (r => `${r}5`)}>5</button>
+                        <button className="button" onClick={e => setValue (r => `${r}6`)}>6</button>
+                        <button className="button" onClick={e => setValue (r => `${r}+`)}>+</button>
+                    </div>
+                    <div className="row">
+                        <button className="button" onClick={e => setValue (r => `${r}1`)}>1</button>
+                        <button className="button" onClick={e => setValue (r => `${r}2`)}>2</button>
+                        <button className="button" onClick={e => setValue (r => `${r}3`)}>3</button>
+                        <button className="button" onClick={e => setValue (r => `${r}-`)}>-</button>
+                    </div>
+                    <div className="row">
+                        <button className="button" onClick={e => setValue (r => `${r}0`)}>0</button>
+                        <button className="button" onClick={e => setValue (r => `${r}.`)}>.</button>
+                        <button className="button" onClick={e => setValue (r => `${r}%`)}>%</button>
+                        <button className="button" onClick={e => setValue (r => eval(r))}>=</button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Calculator
